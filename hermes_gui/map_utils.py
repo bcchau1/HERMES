Fixing map rotation and UI name correction

hermes_gui/map_utils.py
@@ -2,19 +2,24 @@ import numpy as np
 from PyQt5.QtGui import QImage
 
 def occupancy_grid_to_qimage(msg):
-    width = msg.info.width
-    height = msg.info.height
-    data = np.array(msg.data, dtype=np.int8).reshape((height, width))
+    w, h = msg.info.width, msg.info.height
+    data = np.array(msg.data, dtype=np.int8).reshape((h, w))
 
-    image = np.zeros((height, width), dtype=np.uint8)
+    # rotate data because QImage origin is different from ROS2
+    data = np.rot90(data, k = -1)
+    new_height, new_width = data.shape
+
+    # numpy boolean indexing
+    image = np.zeros((new_height, new_width), dtype=np.uint8)
     image[data == -1] = 127     # unknown
     image[data == 0] = 255      # free
     image[data > 0] = 0         # occupied
 
     return QImage(
         image.data,
-        width,
-        height,
-        width,
+        new_width,
+        new_height,
+        new_width,
         QImage.Format_Grayscale8
-    ).copy()  # copy to prevent dangling memory
+    ).copy()
+